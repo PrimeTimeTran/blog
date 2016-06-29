dec8525c149320fe78e2a58c35123f00ba06a950
Loi Tran <lvt15@my.fsu.edu>
2016-06-30 00:06:25 +0700

    route fixed this time?

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  resources :articles
-
   root "blog#index"
   resources :movies
+end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
