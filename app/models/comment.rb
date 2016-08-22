c18629161300294c6887cbede0e55749754fc3ae
Loi Tran <lvt15@my.fsu.edu>
2016-08-22 11:00:57 +0700

    Making Mai say hello

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,8 @@
-class Comment < ActiveRecord::Base
-  
-  belongs_to :message
-  belongs_to :user
-end
+# class Comment < ActiveRecord::Base
+#
+#   belongs_to :message
+#   belongs_to :user
+# end
+
+
+puts "Hello world!"
