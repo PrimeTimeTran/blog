329b12ab300edab912cd2a69c8885a56ffb18386
Loi Tran <lvt15@my.fsu.edu>
2016-09-26 00:20:01 +0700

    does it work?

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,7 +15,7 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
-$(document).on("page:load ready", function() {
+$(document).on("ready page:load", function() {
   $("#target").on("click", function() {
       $("#main").toggle(1000)
       if($(this).text() == 'Hide Articles' ) {
