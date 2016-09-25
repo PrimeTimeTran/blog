957d7d514a092e05fb87f279672d09e32092fc2d
Loi Tran <lvt15@my.fsu.edu>
2016-09-25 19:28:56 +0700

    added validations to the article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,9 +1,13 @@
 class Article < ActiveRecord::Base
+  validates :title, length: { minimum: 5 }
+  validates :body, length: { minimum: 25 }
   has_many :comments
+
   def self.search(query_hash)
     result = all
     result = result.where("title like ?", "%#{query_hash[:title]}%") if query_hash[:title]
     result = result.where("body like ?", "%#{query_hash[:body]}%") if query_hash[:body]
     result
   end
+
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ActiveRecord::Base
+  validates :body, length: { minimum: 25 }
 
   belongs_to :message
   belongs_to :user
