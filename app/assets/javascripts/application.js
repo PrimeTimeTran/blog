10763346ff68abb323335abef72e113d0ba17eec
Loi Tran <lvt15@my.fsu.edu>
2016-10-05 11:07:04 +0700

    fixed carousel and the navigation bar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -30,23 +30,9 @@ $(document).on("page:load ready", function() {
         $("#bg").css("background-color", "#8F8F8F");
         $(this).hide();
       });
-
-      //$("p").css("border", "4px solid red");
-
-      // Activate Carousel
-      $("#myCarousel").carousel();
-
-      // Enable Carousel Indicators
-      $(".item").click(function(){
-          $("#myCarousel").carousel(1);
-      });
-
-      // Enable Carousel Controls
-      $(".left").click(function(){
-          $("#myCarousel").carousel("prev");
-      });
   });
-  $(document).ready(function(){
+
+  $(document).on("page:load ready",function(){
       // Activate Carousel
       $("#myCarousel").carousel();
 
